f447dae9bea4d2498c796ea9e05adadc9129c716
Lisbaldy de Jesus <lisba7474@gmail.com>
2025-01-08 09:27:24 -0300

    Implement real-time chat functionality with Socket.IO and SWC for TypeScript compilation

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,43 @@
 import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from "socket.io";
+import cors from "cors";
 
-const app = express();
 const port = 3000;
+const app = express();
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World! TS!!! Nuevo 2');
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+
+app.get('/test', (req, res) => {
+  res.send('Hello World! TS! Working!');
+});
+
+server.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
